89d441039459db6506e8966c802a3a0cbc66f3e4
Aidan Lynch <aidantlynch00@gmail.com>
2020-09-29 02:10:59 -0400

    Added PlayerLobby sign in functionality

--- src/main/java/com/webcheckers/app/PlayerLobby.java
+++ src/main/java/com/webcheckers/app/PlayerLobby.java
@@ -1,4 +1,28 @@
 package com.webcheckers.app;
 
+import com.webcheckers.model.Player;
+
+import java.util.ArrayList;
+
 public class PlayerLobby {
+
+    private ArrayList<Player> players;
+
+    public PlayerLobby() {
+        players = new ArrayList<>();
+    }
+
+    public boolean isValidUsername(String username) {
+        for (Player p : players) {
+            if (p.getUsername().equals(username)) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public void addPlayer(Player player) {
+        players.add(player);
+    }
 }
