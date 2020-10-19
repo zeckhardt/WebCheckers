cc5da03e632beb9f15098ed9da46fe0487977189
Zach Eckhardt <zre7677@rit.edu>
2020-10-19 13:32:07 -0400

    Updated and added unit tests

--- src/test/java/com/webcheckers/app/PlayerLobbyTest.java
+++ src/test/java/com/webcheckers/app/PlayerLobbyTest.java
@@ -1,6 +1,7 @@
 package com.webcheckers.app;
 import java.util.ArrayList;
 import com.webcheckers.model.Player;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
@@ -12,24 +13,31 @@ import static org.junit.jupiter.api.Assertions.*;
  */
 @Tag("Application-Tier")
 public class PlayerLobbyTest {
+    private PlayerLobby CuT;
+
+    @BeforeEach
+    public void setup(){ CuT = new PlayerLobby();}
 
     @Test
-    public void test_isValidUsername() {
+    public void ctor(){final PlayerLobby CuT = new PlayerLobby();
+    }
+
+    @Test
+    public void testIsValidUsername() {
         final PlayerLobby CuT = new PlayerLobby();
         assertFalse(CuT.isValidUsername("@#$&*#"),"Expected True, got False");
     }
 
     @Test
-    public void test_addPlayer() {
-        final PlayerLobby CuT = new PlayerLobby();
+    public void testAddPlayer() {
         Player player = new Player("name");
+        ArrayList<Player> players = CuT.getPlayers();
         CuT.addPlayer(player);
-        assertEquals(player,player, "Invalid Player");
+        assertEquals(1,players.size(), "Invalid Player");
     }
 
     @Test
-    public void test_getPlayer() {
-        final PlayerLobby CuT = new PlayerLobby();
+    public void testGetPlayer() {
         ArrayList<Player> testAR = new ArrayList<>();
         Player test = new Player("player1");
         Player test2 = new Player("player2");
@@ -38,5 +46,14 @@ public class PlayerLobbyTest {
         CuT.addPlayer(test);
         CuT.addPlayer(test2);
         assertEquals(testAR, CuT.getPlayers(), "Player input invalid");
+    }
+
+    @Test
+    public void testGetPlayerByName() {
+        ArrayList<Player> players = CuT.getPlayers();
+        Player test = new Player("p1");
+        CuT.addPlayer(test);
+        assertNotNull(CuT.getPlayerByName("p1"),"Expected no null, got null return");
+
     }
 }
