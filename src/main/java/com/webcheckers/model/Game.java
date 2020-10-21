f70773c520631f8a5878a8d2b30aeff5d82f175e
Klaus Curde <kcurde@gmail.com>
2020-10-21 15:28:12 -0400

    added documentation

--- src/main/java/com/webcheckers/model/Game.java
+++ src/main/java/com/webcheckers/model/Game.java
@@ -2,6 +2,9 @@ package com.webcheckers.model;
 
 import java.util.UUID;
 
+/**
+ * A class to represent a game object.
+ */
 public class Game {
 
     private UUID uuid;
@@ -10,6 +13,13 @@ public class Game {
     private Player whitePlayer;
     private Player.Color currentTurn;
 
+    /**
+     * Constructor
+     * @param uuid unique identifier for the game
+     * @param board the board object
+     * @param redPlayer the player playing as red
+     * @param whitePlayer the other player playing as white
+     */
     public Game(UUID uuid, Board board, Player redPlayer, Player whitePlayer) {
         this.uuid = uuid;
         this.board = board;
@@ -18,26 +28,49 @@ public class Game {
         this.currentTurn = Player.Color.RED;
     }
 
+    /**
+     * Gets the unique identifier for the game
+     * @return the unique id
+     */
     public UUID getUUID() {
         return uuid;
     }
 
+    /**
+     * Gets the board being used in the game
+     * @return the board object
+     */
     public Board getBoard() {
         return board;
     }
 
+    /**
+     * Gets whose turn it currently is
+     * @return an enum of the Player's turn
+     */
     public Player.Color getCurrentTurn() {
         return currentTurn;
     }
 
+    /**
+     * Changes the current turn from RED to WHITE, or from WHITE to RED
+     */
     public void changeTurn() {
         currentTurn = (currentTurn == Player.Color.RED)? Player.Color.WHITE : Player.Color.RED;
     }
 
+    /**
+     * Gets the player that is playing as red
+     * @return red player object
+     */
     public Player getRedPlayer() {
         return redPlayer;
     }
 
+    /**
+     * Gets the player that is playing as white
+     * @return white player object
+     */
     public Player getWhitePlayer() {
         return whitePlayer;
     }
