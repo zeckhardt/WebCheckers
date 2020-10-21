c428166b8d6746f2108862cda024bde6adb77f36
Zach Eckhardt <zre7677@rit.edu>
2020-10-21 16:41:37 -0400

    postsigninroute test

--- src/test/java/com/webcheckers/ui/PostSignInRouteTest.java
+++ src/test/java/com/webcheckers/ui/PostSignInRouteTest.java
@@ -3,12 +3,17 @@ package com.webcheckers.ui;
 import com.webcheckers.app.PlayerLobby;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Tag;
+import spark.ModelAndView;
 import spark.Request;
+import spark.Response;
+import spark.Session;
 import spark.TemplateEngine;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 /**
  * The unit test for {@link PostSignInRoute}
@@ -22,14 +27,32 @@ public class PostSignInRouteTest {
 
     // mock objects
     private Request request;
+    private Response response;
     private TemplateEngine templateEngine;
     private PlayerLobby playerLobby;
+    private Session session;
 
     @BeforeEach
     public void setup() {
         request = mock(Request.class);
         templateEngine = mock(TemplateEngine.class);
         playerLobby = mock(PlayerLobby.class);
+        response = mock(Response.class);
+        session = mock(Session.class);
         CuT = new PostSignInRoute(templateEngine, playerLobby);
+    }
+
+    @Test
+    public void testHandle(){
+        final TemplateEngineTester testHelper = new TemplateEngineTester();
+        when(templateEngine.render(any(ModelAndView.class))).thenAnswer(testHelper.makeAnswer());
+
+        String test = "test name";
+        CuT.handle(request,response);
+
+        testHelper.assertViewModelExists();
+        testHelper.assertViewModelIsaMap();
+
+
     }
 }
